03f4d885e3b9e96504c395d28d8610fa977c1c49
Glitch (southern-heavy-gray) <none>
2020-07-24 19:27:55 +0000

    🕰🚐 Updated with Glitch

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = require('./Web/index')
-const port = process.env.PORT || 80
+const port = process.env.PORT
 app.listen(port, async function () {
     console.log(`Logado na porta ${port}`)
 });
+/*
+const cache = require('./cache')
+;(async ()=>{
+    await cache.init();
+    //await cache.setItem('name',{test: 2, a: 'c'})
+    let value = await cache.getItem('name')
+    console.log(value.test);
+})()
+*/
